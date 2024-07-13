adf26a75e8172e5a3ceca694af3a9f6d4c175600
Spandan Pankaj Marathe <spandanmarathe95@gmail.com>
2024-07-13 18:30:01 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Listen on all addresses, including external IPs
-    port: process.env.PORT || 3000, // Use the port provided by Render or fallback to 3000
-  }
+  plugins: [react()]
 })
